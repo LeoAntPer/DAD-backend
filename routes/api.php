66c93d621d8054fb9aadfdcd1e3245c91cec5a5c
Joel Batista <2210996@my.ipleiria.pt>
2023-11-22 21:25:37 +0000

    fix problemas login;

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\auth\AuthController;
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
