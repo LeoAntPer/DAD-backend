97df8a0594faabcc6a2462d58966f850244270ff
Leandro Pereira <2211053@my.ipleiria.pt>
2023-11-27 16:10:05 +0000

    User Routes

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,25 @@
 
 use App\Http\Controllers\auth\AuthController;
 use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\DefaultCategoryController;
+use App\Http\Controllers\api\TransactionController;
+use App\Http\Controllers\api\UserController;
+use App\Http\Controllers\api\VcardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->group(function() {
+    Route::apiResource('categories', CategoryController::class);
+});
+
+Route::get('users', [UserController::class, 'index']);
+Route::get('users/{user}', [UserController::class, 'show']);
+Route::post('users', [UserController::class, 'store']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
+
 Route::middleware('auth:api')->post(
     'logout',
     [AuthController::class, 'auth/logout']
-);
-
-Route::middleware('auth:api')->group(
-    function () {
-        Route::apiResource('categories', CategoryController::class);
-    }
 );
