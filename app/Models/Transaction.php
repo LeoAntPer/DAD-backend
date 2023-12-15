ab4776619d26855706b64e5e1194ac2f5c04adeb
Joel Batista <2210996@my.ipleiria.pt>
2023-12-15 10:37:06 +0000

    criar transacao implementado

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -11,7 +11,7 @@ class Transaction extends Model
     use HasFactory;
     use SoftDeletes;
 
-    public $timestamps = false;
+    public $timestamps = true;
 
     protected $fillable = [
         'vcard',
