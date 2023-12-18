cdd5722da5bec5cc27edb20db9bbcb6a3d1a502d
PedroShan <2210904@my.ipleiria.pt>
2023-12-18 10:05:04 +0000

    correções no websocket

--- websocker_server/index.js
+++ websocker_server/index.js
@@ -13,19 +13,8 @@ httpServer.listen(5174, () => {
 io.on("connection", (socket) => {
     console.log(`client ${socket.id} has connected`);
 
-    /*
-    socket.on("joinRoom", (room) => {
-        socket.join(room);
-    });
-
-    socket.on("leaveRoom", (room) => {
-        socket.leave(room);
-    });
-*/
     socket.on('newTransaction', function (transaction) {
-        //socket.join(transaction.payment_reference)
         socket.in('vcard').emit('newTransaction', transaction)
-        //socket.leave(transaction.payment_reference)
     })
 
     socket.on('insertedUser', function (user) {
@@ -33,12 +22,12 @@ io.on("connection", (socket) => {
     })
 
     socket.on('updatedUser', function (user) {
-        console.log(user)
         socket.in('administrator').emit('updatedUser', user)
     })
 
     socket.on('loggedIn', function (user) {
         socket.join(user.id)
+        //console.log(user.id)
         if (user.user_type == 'A') {
             socket.join('administrator')
         } else {
@@ -48,5 +37,6 @@ io.on("connection", (socket) => {
     socket.on('loggedOut', function (user) {
         socket.leave(user.id)
         socket.leave('administrator')
+        socket.leave('vcard')
     })
 });
